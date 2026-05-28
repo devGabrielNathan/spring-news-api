80d1b9c3125c2a865763cd31e8ae9da3ef0f96c4
Leonardo <153339532+lpatros@users.noreply.github.com>
2026-05-28 13:55:14 +0000

    refactor(entity): rename AuthorEntity to Author
    - Simplify class name for better clarity
    - Add Lombok annotations (@Data, @AllArgsConstructor, @NoArgsConstructor)
    - Keep all fields and structure intact

--- src/main/java/br/com/news/entity/Author.java
+++ src/main/java/br/com/news/entity/Author.java
@@ -1,11 +1,9 @@
 package br.com.news.entity;
-import br.com.news.utils.SignatureStatus;
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -14,7 +12,8 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthorEntity {
+public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -36,9 +35,6 @@ public class AuthorEntity {
 
     @Column(nullable = false, length = 255)
     private String signature;
-
-    @Column(nullable = false)
-    private SignatureStatus status;
 
     @Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
     private boolean isEditor;
